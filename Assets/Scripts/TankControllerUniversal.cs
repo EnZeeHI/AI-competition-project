b9595d86a695c3c48bd3c0cae7862f77fe751620
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2019-02-14 16:49:39 +0100

    Initial Commit
    -Set up the environment
    -Set up the universal movement script
    -Set up placeholders for tanks

---
+++ Assets/Scripts/TankControllerUniversal.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControllerUniversal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool LeftBodyRotation;
+    public bool RightBodyRotation;
+    public bool ForwardBodyMovement;
+    public bool BackwardsBodyMovement;
+    public bool FirePrimary;
+    public bool FireSecondary;
+    public bool LeftTurretRotation;
+    public bool RightTurretRotation;
+    private Rigidbody AgentRigidBody;
+    private Collider AgentCollider;
+    public float RotationSpeed;
+    public float MovementSpeed;
+    public GameObject Turret;
+    private Rigidbody TurretRigidbody;
+
+
+
+    void Start()
+    {
+        AgentRigidBody = GetComponent<Rigidbody>();
+        AgentCollider = GetComponent<BoxCollider>();
+
+        
+        TurretRigidbody = Turret.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LeftBodyRotation)
+        {
+            AgentRigidBody.transform.Rotate(Vector3.down*Time.deltaTime*RotationSpeed);
+
+        }
+        if (RightBodyRotation)
+        {
+            AgentRigidBody.transform.Rotate(Vector3.up*Time.deltaTime*RotationSpeed);
+        }
+        if (ForwardBodyMovement)
+        {
+            AgentRigidBody.transform.Rotate(Vector3.left*Time.deltaTime*MovementSpeed);
+        }
+        if (BackwardsBodyMovement)
+        {
+            AgentRigidBody.transform.Rotate(Vector3.right*Time.deltaTime*MovementSpeed);
+        }
+        if (FirePrimary)
+        {
+            
+        }
+        if (FireSecondary)
+        {
+
+        }
+        if (LeftTurretRotation)
+        {
+            TurretRigidbody.transform.Rotate(Vector3.down*Time.deltaTime*RotationSpeed);
+        }
+        if (RightTurretRotation)
+        {
+            TurretRigidbody.transform.Rotate(Vector3.up*Time.deltaTime*RotationSpeed);
+        }
+
+        
+    }
+}
