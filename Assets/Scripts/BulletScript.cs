92be0f07c441cc49d5545f2afb0d0a7af58f6285
TeraBitGaming <loermansw@gmail.com>
2019-02-18 10:54:26 +0100

    Wesley's demo - initial commit
    Tanks kinda work, need to add hooks to control the movement through scripts.

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update(){
+        GetComponent<Rigidbody>().velocity = transform.up * 20;
+    }
+
+    void OnCollisionEnter(Collision collision){
+        collision.gameObject.GetComponent<TankControlScript>().damage();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TankControlScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControlScript : MonoBehaviour {
+
+    public GameObject tankTreads;
+    public GameObject tankTurret;
+    public GameObject shooty;
+    private GameObject bullet;
+    public GameObject Projectile;
+    private Rigidbody rB;
+    float turretRotation = 0f;
+    float treadRotation = 0f;
+    float moveSpeed = 0f;
+    public bool control = false;
+
+    public void damage(){
+        Debug.Log("ouch");
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        rB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (control == true){
+            turretRotation += Input.GetAxis("Turret") * 3;
+            tankTurret.transform.eulerAngles = new Vector3(0, turretRotation, 0);
+            treadRotation += Input.GetAxis("Horizontal") * 2;
+            tankTreads.transform.eulerAngles = new Vector3(0, treadRotation, 0);
+            moveSpeed = Input.GetAxis("Vertical") * -10;
+            rB.velocity = tankTreads.transform.forward * moveSpeed;
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                GameObject bullet = Instantiate(Projectile, shooty.transform.position, shooty.transform.rotation) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward);
+            }
+        }
+    }
+}
