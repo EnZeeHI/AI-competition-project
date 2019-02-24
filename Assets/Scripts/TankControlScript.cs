4891f9d8006579be5017f84240585d08a9862fca
TeraBitGaming <loermansw@gmail.com>
2019-02-24 14:16:18 +0100

    Mpvement v:1.0
    First iteration, not final.
    Need to figure out why the turret no longer moves independently from the body.
    Need to figure out a way to keep the bullets from firing so f'n much (probably an if loop with += integer?)

--- Assets/Scripts/TankControlScript.cs
+++ Assets/Scripts/TankControlScript.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class TankControlScript : MonoBehaviour {
 
+    // defining possible tank actions
+    public bool LeftBodyRotation;
+    public bool RightBodyRotation;
+    public bool ForwardBodyMovement;
+    public bool BackwardsBodyMovement;
+    public bool FirePrimary;
+    // public bool FireSecondary;
+    public bool LeftTurretRotation;
+    public bool RightTurretRotation;
+    
+    // defining gameobjects and other things that take parameters
     public GameObject tankTreads;
     public GameObject tankTurret;
     public GameObject shooty;
@@ -13,7 +24,10 @@ public class TankControlScript : MonoBehaviour {
     float turretRotation = 0f;
     float treadRotation = 0f;
     float moveSpeed = 0f;
-    public bool control = false;
+    private bool canShoot = true;
+
+    // bool for testing/debug purposes
+    public bool aiControl = false;
 
     public void damage(){
         Debug.Log("ouch");
@@ -26,17 +40,46 @@ public class TankControlScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update(){
-        if (control == true){
+        if (aiControl == false){
             turretRotation += Input.GetAxis("Turret") * 3;
             tankTurret.transform.eulerAngles = new Vector3(0, turretRotation, 0);
+
             treadRotation += Input.GetAxis("Horizontal") * 2;
             tankTreads.transform.eulerAngles = new Vector3(0, treadRotation, 0);
+
             moveSpeed = Input.GetAxis("Vertical") * -10;
             rB.velocity = tankTreads.transform.forward * moveSpeed;
+
             if (Input.GetKeyDown(KeyCode.Space)) {
                 GameObject bullet = Instantiate(Projectile, shooty.transform.position, shooty.transform.rotation) as GameObject;
                 bullet.GetComponent<Rigidbody>().AddForce(transform.forward);
             }
+        }
+        if (aiControl == true){
+            if (LeftBodyRotation) {
+                tankTreads.transform.eulerAngles += new Vector3(0, -2, 0);
+            }
+            if (RightBodyRotation) {
+                tankTreads.transform.eulerAngles += new Vector3(0, 2, 0);
+            }
+            if (ForwardBodyMovement) {
+                rB.velocity = tankTreads.transform.forward * -10;
+            }
+            if (BackwardsBodyMovement) {
+                rB.velocity = tankTreads.transform.forward * 10;
+            }
+            if (FirePrimary) {
+                canShoot = false;
+                GameObject bullet = Instantiate(Projectile, shooty.transform.position, shooty.transform.rotation) as GameObject;
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward);
+            }
+            if (LeftTurretRotation) {
+                tankTurret.transform.eulerAngles += new Vector3(0, -3, 0);
+            }
+            if (RightTurretRotation) {
+                tankTurret.transform.eulerAngles += new Vector3(0, 3, 0);
+            }
+            canShoot = true;
         }
     }
 }
