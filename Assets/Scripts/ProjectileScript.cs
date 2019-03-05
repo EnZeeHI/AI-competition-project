6bc3612f94a9f8cbbb98a7113ea3140d8e8c339d
EnZeeHI <nazarovas14@gmail.com>
2019-03-05 13:01:40 +0100

    added ricochet functionality to the bullet

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -9,6 +9,7 @@ public class ProjectileScript : MonoBehaviour
     private TankControllerUniversal TankScript;
     public int AttackDamage;
     public Collider AgentCollider;
+    public int collisionCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +18,26 @@ public class ProjectileScript : MonoBehaviour
         TankScript = Tank.GetComponentInChildren<TankControllerUniversal>();
         AgentCollider = Tank.GetComponentInChildren<BoxCollider>();
     }
+    // checks for collision, if the bullet doesnt hit a tank it ricochets once
     void OnCollisionEnter(Collision AgentCollider) 
     {   
         if (AgentCollider.gameObject.tag =="Tank")
         {
             TankScript.GiveDamage(AgentCollider, AttackDamage);
-            
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+        else
+        {
+            
+            if (collisionCounter >= 1)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                collisionCounter = collisionCounter+1;
+            }
+        }
+       
     }
 }
